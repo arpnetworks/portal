f73e0990b5a41c0247d6bd71a05e24d5d7c66307
Garry Dolley <gdolley@arpnetworks.com>
2017-07-26 01:52:52 -0700

    Added Jobs::CreateVolumeFromTemplateForVM

---
+++ app/models/jobs/create_volume_from_template_for_vm.rb
@@ -0,0 +1,19 @@
+class Jobs::CreateVolumeFromTemplateForVM < Jobs::CreateVolume
+  def perform(args_json)
+    args = JSON.parse(args_json)
+
+    vm   = args['vm']['virtual_machine']
+    opts = args['opts'] || {}
+
+    opts.merge!(extract_ops(vm))
+    opts.merge!(:os_template => vm['os_template'])
+
+    super({ :account_id => args['account_id'],
+            :host       => vm['host'],
+            :vol_name   => vm['label'],
+            :size       => vm['storage'],
+            :opts       => opts,
+            :orig_args_json => args_json
+          }.to_json)
+  end
+end
