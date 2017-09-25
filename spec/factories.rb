8600e8e98c1c88bd2d4195a6ebc6428977081c6e
Garry Dolley <gdolley@arpnetworks.com>
2017-09-25 02:56:13 -0700

    Brought up-to-date with latest Factory Girl

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,91 +1,107 @@
-Factory.define :account do |a|
-  a.sequence(:login) { |n| "foo#{n}" }
-  a.sequence(:email) { |n| "foo#{n}@example.com" }
-  a.password 'password'
-  a.password_confirmation { |a| a.password }
-end
-
-Factory.define :service do |s|
-  s.association :account
-  s.association :service_code
-  s.billing_amount 5.00
-end
-
-Factory.define :service_code do |sc|
-end
-
-Factory.define :service_code_for_ip_block, :parent => :service_code do |sc|
-  sc.name 'IP_BLOCK'
-end
-
-Factory.define :resource do |r|
-  r.association :service
-end
-
-Factory.define :ip_block do |ip|
-  ip.cidr '10.0.0.0/30'
-  ip.vlan '999'
-  ip.after_create do |ip|
-    ip.resource = Factory.create(:resource, :assignable => ip)
+FactoryGirl.define do
+  sequence :login do |n|
+    "foo#{n}"
   end
-end
 
-Factory.define :virtual_machine do |vm|
-  vm.uuid '682950e4-4af8-11e0-8cea-001c25748b20'
-  vm.label 'foo'
-  vm.after_create do |vm|
-    vm.resource = Factory.create(:resource, :assignable => vm)
+  sequence :email do |n|
+    "foo#{n}@example.com"
   end
-end
 
-Factory.define :bandwidth_quota do |bq|
-  bq.cacti_username 'johndoe'
-  bq.cacti_password 'peeword'
-  bq.cacti_local_graph_id 1000
-  bq.after_create do |bq|
-    bq.resource = Factory.create(:resource, :assignable => bq)
+  factory :account do
+    login
+    email
+    password 'password'
+    password_confirmation 'password'
   end
-end
 
-Factory.define :dns_domain do |d|
-  d.name '0.0.10.in-addr.arpa'
-end
-
-Factory.define :dns_record do |r|
-  r.association :dns_domain
-  r.name '2.0.0.10.in-addr.arpa'
-  r.content 'example.com'
-  r.after_build do |r|
-    r.type = 'PTR'
+  factory :service do
+    account
+    service_code
+    billing_amount 5.00
   end
-end
 
-Factory.define :dns_record_with_ns_type, :class => DnsRecord do |r|
-  r.association :dns_domain
-  r.name '2.0.0.10.in-addr.arpa'
-  r.content 'example.com'
-  r.after_build do |r|
-    r.type = 'NS'
+  factory :service_code do
+    factory :service_code_for_ip_block do
+      name 'IP_BLOCK'
+    end
   end
-end
 
-Factory.define :backup_quota do |bq|
-  bq.server 'backup01.cust.arpnetworks.com'
-  bq.username 'garry'
-  bq.group 'sftpusers'
-  bq.quota 20000000
-  bq.home_dir '/home/sftpusers/home/garry'
-  bq.chroot_dir '/home/sftpusers/home'
-  bq.notes ''
-end
+  factory :resource do
+    service
+  end
 
-Factory.define :credit_card do |cc|
-  cc.association :account
-  cc.display_number '1111'
-  cc.number 4111111111111111
-  cc.month '12'
-  cc.year '2036'
-  cc.first_name 'John'
-  cc.last_name 'Doe'
-  cc.billing_country_iso_3166 'US'
+  factory :location do
+    name 'Los Angeles'
+    code 'lax'
+  end
+
+  factory :ip_block do
+    location
+    cidr '10.0.0.0/30'
+    vlan '999'
+    after(:create) do |ip|
+      ip.resource = create(:resource, assignable: ip)
+    end
+
+    trait :available do
+      available true
+    end
+  end
+
+  factory :virtual_machine do
+    uuid '682950e4-4af8-11e0-8cea-001c25748b20'
+    label 'foo'
+    after(:create) do |vm|
+      vm.resource = create(:resource, assignable: vm)
+    end
+  end
+
+  factory :bandwidth_quota do
+    cacti_username 'johndoe'
+    cacti_password 'peeword'
+    cacti_local_graph_id 1000
+    after(:create) do |bq|
+      bq.resource = create(:resource, assignable: bq)
+    end
+  end
+
+  factory :dns_domain do
+    name '0.0.10.in-addr.arpa'
+  end
+
+  factory :dns_record do
+    dns_domain
+    name '2.0.0.10.in-addr.arpa'
+    content 'example.com'
+    after(:build) do |r|
+      r.type = 'PTR'
+    end
+
+    factory :dns_record_with_ns_type do
+      after(:build) do |r|
+        r.type = 'NS'
+      end
+    end
+  end
+
+  factory :backup_quota do
+    server 'backup01.cust.arpnetworks.com'
+    username 'garry'
+    group 'sftpusers'
+    quota 20000000
+    home_dir '/home/sftpusers/home/garry'
+    chroot_dir '/home/sftpusers/home'
+    notes ''
+  end
+
+  factory :credit_card do
+    association :account
+    display_number '1111'
+    number 4111111111111111
+    month '12'
+    year '2036'
+    first_name 'John'
+    last_name 'Doe'
+    billing_country_iso_3166 'US'
+  end
 end
