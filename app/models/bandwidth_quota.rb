5a5e3dad30e20d77afb29c53265449b3f649abcd
Garry Dolley <gdolley@arpnetworks.com>
2018-05-16 02:38:21 -0700

    Added BandwidthQuota model

---
+++ app/models/bandwidth_quota.rb
@@ -0,0 +1,17 @@
+class BandwidthQuota < ActiveRecord::Base
+  self.table_name = 'bandwidth_quotas'
+
+  include Resourceable
+
+  validates_numericality_of :commit
+  validates_numericality_of :cacti_local_graph_id, allow_nil: true
+  validates_format_of       :commit_unit, with: /\A(Mbps|GB)\Z/
+
+  def graph_url
+    if cacti_local_graph_id.to_s.empty?
+      "https://graphs.arpnetworks.com/cacti"
+    else
+      "https://graphs.arpnetworks.com/cacti/graph.php?local_graph_id=#{cacti_local_graph_id}&rra_id=all"
+    end
+  end
+end
