35d73cc23c3193d9eee0174a8b9b34597aa68396
Garry Dolley <gdolley@arpnetworks.com>
2018-05-17 02:16:30 -0700

    Stock my_account_controller.rb

---
+++ app/controllers/my_account_controller.rb
@@ -0,0 +1,26 @@
+class MyAccountController < ProtectedController
+  before_filter :default_params, :except => [:edit]
+
+  def dashboard
+    expiry = Account.tender_token_expiry_timestamp
+    url_encoded_email = CGI::escape(@account.email)
+    @tender_link = "http://support.arpnetworks.com/login?email=#{url_encoded_email}&expires=#{expiry}&hash=#{@account.tender_token(expiry)}"
+    @services = @account.services.active
+
+    # If we wanted all unpaid invoices to appear on top
+    # @invoices = @account.invoices.active.find(:all, :order => 'paid asc, date desc', :limit => 5)
+
+    @invoices = @account.invoices.active.find(:all, :order => 'date desc', :limit => 5)
+    @unpaid_invoices = @account.invoices_unpaid
+    @enable_summary_view = true
+
+    @jobs = @account.jobs.recent.find(:all, :order => 'created_at desc', :limit => 5)
+  end
+
+  protected
+
+  def default_params
+    @account  = Account.find(session[:account_id])
+    @per_page = (params[:per_page] ||= 10).to_i
+  end
+end
