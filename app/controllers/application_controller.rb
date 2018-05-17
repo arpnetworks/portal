807046cfa796567e43e4b4256cfc8867da09b80f
Garry Dolley <gdolley@arpnetworks.com>
2018-05-17 01:28:38 -0700

    New ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  # TODO
+  # include ExceptionNotifiable
+
+  before_filter :clean_slate
+
+  # TODO: Do we still need this?
+  # helper :all # include all helpers, all the time
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def clean_slate
+    @enable_admin_view = false
+  end
 end
