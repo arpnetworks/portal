aa5a1de259a69afc00e389df6c7101edfdc0b9dc
Garry Dolley <gdolley@arpnetworks.com>
2018-05-17 00:51:51 -0700

    Routes copied verbatim from old Portal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,128 @@
 Rails.application.routes.draw do
+  # ===========================
+  # BEGIN: Copy from old Portal
+  # ===========================
+
+  map.resources :accounts, :collection => { :forgot_password      => :get,
+                                            :forgot_password_post => :post,
+                                            :login                => :get,
+                                            :login_attempt        => :post,
+                                            :logout               => :get
+                                           } do |accounts|
+    accounts.resources :services, {
+      :collection => {
+        :confirm      => :post,
+        :confirm_done => :post,
+        :remove       => :get
+      },
+      :member => {
+        :update_label => :put }} do |services|
+      services.resources :virtual_machines, :member => {
+        :boot          => :post,
+        :shutdown      => :post,
+        :shutdown_hard => :post,
+        :ssh_key       => :get,
+        :ssh_key_post  => :post,
+        :iso_change    => :post,
+        :advanced_parameter => :post
+      }
+      services.resources :ip_blocks
+      services.resources :bandwidth_quotas
+      services.resources :backup_quotas, :member => {
+        :ssh_key       => :get,
+        :ssh_key_post  => :post
+      }
+      services.resources :bgp_sessions
+    end
+
+    accounts.resources :dns_records, :collection => {
+      :reverse_dns => :get
+    }
+
+    accounts.resources :credit_cards
+    accounts.resources :invoices, :collection => {
+      :pay         => :get,
+      :pay_confirm => :post
+    }
+    accounts.resources :jobs
+  end
+
+  map.connect '/noVNC/console', :controller => 'virtual_machines', :action => 'console'
+
+  map.dashboard '/dashboard', :controller => 'my_account', :action => 'dashboard'
+
+  map.namespace(:admin) do |admin|
+    admin.resources :accounts
+    admin.resources :invoices
+    admin.resources :services
+    admin.resources :service_codes
+    admin.resources :virtual_machines,
+      :member => {
+        :monitoring_reminder_post => :post }
+    admin.resources :ip_blocks, :collection => { :tree   => :get },
+                                :member     => { :subnet => :get,
+                                                 :swip   => :get,
+                                                 :swip_submit => :post }
+    admin.resources :bandwidth_quotas
+    admin.resources :backup_quotas
+    admin.resources :bgp_sessions
+    admin.resources :bgp_sessions_prefixes
+    admin.resources :vlans, :member => {
+      :shutdown => :post,
+      :restore  => :post
+    }
+    admin.resources :jobs, :member => {
+      :retry => :post
+    }
+  end
+  map.admin "admin", :controller => 'admin/head_quarters', :action => 'index'
+
+  # Switch user and search
+  map.connect "admin/su", :controller => 'admin/head_quarters', :action => 'su'
+  map.connect "admin/search", :controller => 'admin/head_quarters', :action => 'search'
+
+  # Reports
+  map.connect "admin/reports/services", :controller => 'admin/reports', :action => 'services'
+
+  # Other
+  map.connect "admin/whoami", :controller => 'admin/head_quarters', :action => 'whoami'
+
+  # Internal API
+  map.namespace(:api) do |api|
+    api.namespace(:v1) do |v1|
+      v1.namespace(:internal) do |internal|
+        internal.connect 'jobs/:id/event/:event',        :controller => 'jobs', :action => 'event', :conditions => { :method => :put }
+        internal.connect 'jobs/:id/event/:event/:args',  :controller => 'jobs', :action => 'event', :conditions => { :method => :put }
+
+        internal.connect 'virtual_machines/:uuid/status/:status', :controller => 'virtual_machines',
+                                                                  :action     => 'status',
+                                                                  :conditions => { :method => :put }
+        internal.connect 'virtual_machines/statuses', :controller => 'virtual_machines',
+                                                      :action     => 'statuses',
+                                                      :conditions => { :method => :put }
+
+        # Provisioning
+        internal.connect 'provisioning/:mac_address/config.tar.gz', :controller => 'provisioning',
+                                                                    :action     => 'config'
+
+        # Legacy
+        internal.connect 'console_logins',            :controller => 'utils', :action => 'console_logins'
+        internal.connect 'console_passwd_file',       :controller => 'utils', :action => 'console_passwd_file'
+        internal.connect 'console_passwd_file/:host', :controller => 'utils', :action => 'console_passwd_file'
+        internal.connect 'redis/ping', :controller => 'utils', :action => 'redis_ping'
+
+        # Other
+        internal.connect 'jobs/health', :controller => 'utils', :action => 'job_queue_health'
+      end
+    end
+  end
+
+  map.root :controller => 'accounts', :action => 'login'
+
+  # =========================
+  # END: Copy from old Portal
+  # =========================
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
