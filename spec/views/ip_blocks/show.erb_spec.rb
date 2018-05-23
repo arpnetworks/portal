868643e54d10fd958ffa7df524c0d7e418c71e69
Garry Dolley <gdolley@arpnetworks.com>
2018-05-23 04:08:57 -0700

    Added first view spec

---
+++ spec/views/ip_blocks/show.erb_spec.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + '/../../rails_helper'
+
+describe "/services/show.erb" do
+  include RSpecHtmlMatchers
+
+  context "description is empty" do
+    before do
+      ip_block = build :ip_block do |ipb|
+        ipb.notes = ''
+      end
+      assign(:ip_blocks, [ip_block])
+    end
+
+    it "should not display 'Additional' section" do
+      render "/ip_blocks/table_data.erb"
+      expect(response).to_not have_tag('td.th-minor', text: /Additional/)
+    end
+  end
+
+  context "description is not empty" do
+    before do
+      ip_block = build :ip_block do |ipb|
+        ipb.notes = 'lkjsdf'
+      end
+      assign(:ip_blocks, [ip_block])
+    end
+
+    it "should not display 'Additional' section" do
+      render "/ip_blocks/table_data.erb"
+      expect(response).to have_tag('td.th-minor', text: /Additional/)
+    end
+  end
+end
+
