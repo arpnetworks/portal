1db1b857a3c394701a2bdcce7cab114e3a936fbd
Garry Dolley <gdolley@arpnetworks.com>
2018-05-23 16:47:51 -0700

    ...and more view specs

---
+++ spec/views/admin/service_codes/edit.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../../rails_helper')
+
+describe "/admin/service_codes/edit.html.erb" do
+  include RSpecHtmlMatchers
+
+  before(:each) do
+    @service_code = stub_model(ServiceCode)
+    assign(:service_code, @service_code)
+  end
+
+  it "should render edit form" do
+    render template: "/admin/service_codes/edit.html.erb"
+
+    expect(response).to have_tag("form[action='#{admin_service_code_path(@service_code)}'][method=post]")
+  end
+end
+
+
---
+++ spec/views/admin/service_codes/show.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../../rails_helper')
+
+describe "/admin/service_codes/show.html.erb" do
+  before(:each) do
+    assign(:service_codes, [
+      stub_model(ServiceCode),
+      stub_model(ServiceCode)
+    ])
+  end
+end
+
