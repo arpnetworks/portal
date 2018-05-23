8cc156f916090db9bfe98b93175806fbd1298abc
Garry Dolley <gdolley@arpnetworks.com>
2018-05-23 04:47:30 -0700

    Retrofitted for Rails 4

--- spec/views/invoices/pay.erb_spec.rb
+++ spec/views/invoices/pay.erb_spec.rb
@@ -1,38 +1,39 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../my_spec_helper')
+require File.dirname(__FILE__) + '/../../rails_helper'
 
 describe '/invoices/pay.erb' do
+  include RSpecHtmlMatchers
+
   before do
     @account = stub_model(Account)
-    assigns[:account] = @account
+    assign(:account, @account)
   end
 
   context 'account does not have unpaid invoices' do
     before do
-      assigns[:outstanding_balance] = 0
+      assign(:outstanding_balance, 0)
     end
 
     it 'should display thank you greeting' do
-      render '/invoices/pay.erb'
-      response.should have_tag("div[class=notice-green]", /All invoices are paid.*Thank you/)
+      render template: '/invoices/pay.erb'
+      expect(response).to have_tag("div[class=notice-green]", text: /All invoices are paid.*Thank you/)
     end
   end
 
   context 'account has unpaid invoices' do
     before do
       @outstanding_balance = 444
-      assigns[:outstanding_balance] = @outstanding_balance
+      assign(:outstanding_balance, @outstanding_balance)
     end
 
     context 'account does not have a credit card' do
       before do
-        assigns[:credit_card] = nil
+        assign(:credit_card, nil)
       end
 
       it 'should display button to add credit card' do
-        render '/invoices/pay.erb'
-        response.should have_tag("div[class=notice-green]", /Your account does not have a credit card/)
-        response.should have_tag("a[href=?]", new_account_credit_card_path(@account.id), /Add Credit Card/)
+        render template: '/invoices/pay.erb'
+        expect(response).to have_tag("div[class=notice-green]", text: /Your account does not have a credit card/)
+        expect(response).to have_tag("a[href='%s']" % new_account_credit_card_path(@account.id), text: /Add Credit Card/)
       end
     end
 
@@ -40,51 +41,51 @@ describe '/invoices/pay.erb' do
       before do
         @cc_num = '4111111111111111'
 
-        assigns[:credit_card] = stub_model(CreditCard,
-                                           :number => @cc_num,
-                                           :display_number => '1111')
+        assign(:credit_card, stub_model(CreditCard,
+                                        :number => @cc_num,
+                                        :display_number => '1111'))
       end
 
       it 'should display link to change credit card' do
-        render '/invoices/pay.erb'
-        response.should have_tag("a[href=?]", new_account_credit_card_path(@account.id), /submit a new one/)
+        render template: '/invoices/pay.erb'
+        expect(response).to have_tag("a[href='%s']" % new_account_credit_card_path(@account.id), text: /submit a new one/)
       end
 
       context 'cc_e cookie matches credit card' do
         before do
-          assigns[:credit_card_number] = @cc_num
+          assign(:credit_card_number, @cc_num)
         end
 
         it 'should have hidden input for credit card number' do
-          render '/invoices/pay.erb'
-          response.should have_tag("input[type=hidden][name=credit_card_number][value=?]", @cc_num)
+          render template: '/invoices/pay.erb'
+          expect(response).to have_tag("input[type=hidden][name=credit_card_number][value='%s']" % @cc_num)
         end
       end
 
       context 'cc_e cookie does not match credit card' do
         before do
-          assigns[:credit_card_number] = nil
+          assign(:credit_card_number, nil)
         end
 
         it 'should display input for credit card number' do
-          render '/invoices/pay.erb'
-          response.should have_tag("input[type=text][name=credit_card_number]")
+          render template: '/invoices/pay.erb'
+          expect(response).to have_tag("input[type=text][name=credit_card_number]")
         end
       end
 
       it 'should display current card to use' do
-        render '/invoices/pay.erb'
-        response.should have_tag("div[id=credit-card]", /Credit Card.*\*\*1111/m)
+        render template: '/invoices/pay.erb'
+        expect(response).to have_tag("div[id=credit-card]", text: /Credit Card.*\*\*1111/m)
       end
 
       it 'should display confirmation button' do
-        render '/invoices/pay.erb'
-        response.should have_tag("button[type=submit]", /Authorize/)
+        render template: '/invoices/pay.erb'
+        expect(response).to have_tag("button[type=submit]", text: /Authorize/)
       end
 
       it 'should have hidden input for amount to pay' do
-        render '/invoices/pay.erb'
-        response.should have_tag("input[type=hidden][name=confirmed_amount][value=?]", @outstanding_balance)
+        render template: '/invoices/pay.erb'
+        expect(response).to have_tag("input[type=hidden][name=confirmed_amount][value='%s']" % @outstanding_balance)
       end
     end
   end
