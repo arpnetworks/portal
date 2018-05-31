5607fc76a0a8af65b6d2af0c5ebd7411280b47d1
Garry Dolley <gdolley@arpnetworks.com>
2018-05-30 23:31:58 -0700

    Added app/helpers/virtual_machines_helper.rb

---
+++ app/helpers/virtual_machines_helper.rb
@@ -0,0 +1,13 @@
+module VirtualMachinesHelper
+  def virtual_machines_colspan(admin)
+    admin ? 6 : 5
+  end
+
+  def virtual_machines_table_onClick(vm)
+    if @enable_admin_view
+      html = "onClick=\"location.href='".html_safe
+      html << (@enable_admin_view ? admin_virtual_machine_path(vm.id) : account_service_virtual_machine_path(@account, @service, vm.id))
+      heml << "'\"".html_safe
+    end
+  end
+end
