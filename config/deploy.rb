e5a96a460863cb7ffb22f6721610585575e97e9a
Garry Dolley <gdolley@arpnetworks.com>
2018-06-23 21:19:08 -0700

    Version issues...

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.2"
+lock "~> 3.10"
 
 set :application, "portal"
 set :repo_url, "git@github.com:arpnetworks/portal.git"
@@ -49,7 +49,7 @@ append :linked_dirs, "log",
 # set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
-set :ssh_options, verify_host_key: :secure
+set :ssh_options, verify_host_key: :always
 
 # rbenv
 set :rbenv_type, :user
