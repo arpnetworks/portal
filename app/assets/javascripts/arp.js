d5a9f8f2088edd82b93a87d856c4270d431e14f4
Garry Dolley <gdolley@arpnetworks.com>
2020-05-15 01:06:45 -0700

    Refactored properly so this js doesn't execute on every page

--- app/assets/javascripts/arp.js
+++ app/assets/javascripts/arp.js
@@ -1,44 +1,44 @@
-$(function() {
-  /* ------------------------ */
-  /* NEW SERVICE CONFIGURATOR */
-  /* ------------------------ */
+/* ------------------------ */
+/* NEW SERVICE CONFIGURATOR */
+/* ------------------------ */
 
+function populateIpAddresses(location_code) {
+  $.ajax({
+    dataType: "json",
+    url: "/provisioning/ip_address",
+    data: "location=" + location_code,
+    success: function (data) {
+      ips = data["ips"];
+
+      options = "<option>" + data["caption"] + "</option>";
+
+      $.each(ips, function (k, v) {
+        options += '<option value="' + v["ip_address"] + '"';
+        options += (v["assigned"] ? " disabled" : "") + ">";
+        options += v["ip_address"];
+
+        if (v["assigned"]) {
+          options += " (assigned to " + v["assignment"] + ")";
+        }
+
+        options += "</option>";
+      });
+
+      $("#ipv4_address_selector").html(options);
+    },
+    error: function (data) {
+      alert(
+        "Could not retrieve IP addresses.\nPlease try again later."
+      );
+    },
+  });
+}
+
+$(function () {
   /* Change the IP address drop-down based on the chosen location */
-  $('#new_vps_with_os input[name=location]').change(function() {
-    if($(this).is(":checked")) {
-      populateIpAddresses($(this).val())
+  $("#new_vps_with_os input[name=location]").change(function () {
+    if ($(this).is(":checked")) {
+      populateIpAddresses($(this).val());
     }
-  })
-
-  function populateIpAddresses(location_code) {
-    $.ajax({
-      dataType: 'json',
-      url: '/provisioning/ip_address',
-      data: 'location=' + location_code,
-      success: function(data) {
-        ips = data['ips']
-
-        options = '<option>' + data['caption'] + '</option>'
-
-        $.each(ips, function(k, v) {
-          options += '<option value="' + v['ip_address'] + '"'
-          options += (v['assigned'] ? ' disabled' : '') + '>'
-          options += v['ip_address']
-
-          if(v['assigned']) {
-            options += ' (assigned to ' + v['assignment'] + ')'
-          }
-
-          options += '</option>'
-        })
-
-        $('#ipv4_address_selector').html(options)
-      },
-      error: function(data) {
-        alert("Could not retrieve IP addresses.\nPlease try again later.")
-      }
-    })
-  }
-
-  populateIpAddresses('lax')
+  });
 });
