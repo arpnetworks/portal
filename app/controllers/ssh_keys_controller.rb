f68b00a33e03c994c578cdd5a8ae0050ced4d368
Garry Dolley <gdolley@arpnetworks.com>
2020-05-22 02:44:45 -0700

    Progress on SshKeysController

--- app/controllers/ssh_keys_controller.rb
+++ app/controllers/ssh_keys_controller.rb
@@ -9,6 +9,24 @@ class SshKeysController < ProtectedController
   end
 
   def create
-    @account
+    @name = params[:ssh_key][:name]
+    @key = params[:ssh_key][:key]
+
+    if @key.blank? || @name.blank?
+      respond_to do |format|
+        format.json do
+          render json: {
+            error: "Key name and the actual key are required fields."
+          }, status: :bad_request
+        end
+      end
+      return
+    end
+
+    @account.ssh_keys.create(name: @name, key: @key)
+
+    respond_to do |format|
+      format.json { render json: { message: 'Success' } }
+    end
   end
 end
--- spec/controllers/ssh_keys_controller_spec.rb
+++ spec/controllers/ssh_keys_controller_spec.rb
@@ -19,9 +19,62 @@ describe SshKeysController do
     end
   end
 
-  context 'create()' do
-    def do_create(opts = {})
+  context 'handling POST /accounts/1/ssh_keys' do
+    before do
+      @account = mock_login!
+    end
+
+    def do_post(opts = {})
+      opts[:ssh_key] = {}
+      opts[:ssh_key][:name] ||= @key_name
+      opts[:ssh_key][:key] ||= @key
+
       post :create, { account_id: @account.id }.merge(opts)
+    end
+
+    context 'with JSON' do
+      before do
+        @key_name = 'garry'
+        @key = 'ssh-rsa AAA... me@example.com'
+      end
+
+      context 'with key and key name' do
+        it 'should create a new SSH key' do
+          mock_ssh_keys = mock_model(SshKey)
+          expect(@account).to receive(:ssh_keys).and_return(mock_ssh_keys)
+          expect(mock_ssh_keys).to receive(:create).with(name: @key_name,
+                                                         key: @key)
+          do_post(format: :json)
+          expect(@response).to be_success
+          expect(@response.body).to include('Success')
+        end
+      end
+
+      context 'without key name' do
+        before do
+          @key_name = ''
+        end
+
+        it 'should return an error' do
+          do_post(format: :json)
+
+          expect(@response).to_not be_success
+          expect(@response.body).to include('required fields')
+        end
+      end
+
+      context 'without key' do
+        before do
+          @key = ''
+        end
+
+        it 'should return an error' do
+          do_post(format: :json)
+
+          expect(@response).to_not be_success
+          expect(@response.body).to include('required fields')
+        end
+      end
     end
   end
 end
