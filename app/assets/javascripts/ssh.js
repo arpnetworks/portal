8404eab6285e0fc4a267920b5ef3d5e4413b7e0c
Garry Dolley <gdolley@arpnetworks.com>
2020-05-25 20:31:53 -0700

    Testing our js

--- app/assets/javascripts/ssh.js
+++ app/assets/javascripts/ssh.js
@@ -1,20 +1,37 @@
-function usernameFromPubKey (key) {
-  if (key.length > 32) {
+function usernameFromPubKey(key) {
+  var key = sanitizeKey(key)
 
-  } else {
-    return ''
+  if(key.length > 0) {
+
+
   }
+
+  return "";
 }
 
-function labelFromPubKey (key) {
-  if (key.length > 32) {
+function labelFromPubKey(key) {
+  var key = sanitizeKey(key)
 
-  } else {
-    return ''
+  if(key.length > 0) {
+    key.split(' ')[2]
+  }
+
+  return "";
+}
+
+function sanitizeKey(key) {
+  try {
+    if (key.length > 32) {
+    } else {
+      throw "Key length too small";
+    }
+  } catch (err) {
+    return "";
   }
 }
 
 module.exports = {
   usernameFromPubKey: usernameFromPubKey,
-  labelFromPubKey: labelFromPubKey
-}
+  labelFromPubKey: labelFromPubKey,
+  sanitizeKey: sanitizeKey,
+};
