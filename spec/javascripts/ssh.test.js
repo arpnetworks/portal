3fa99509d361473a359e55e952e177a13ecefb83
Garry Dolley <gdolley@arpnetworks.com>
2020-05-25 20:31:23 -0700

    Enable code coverage in Jest

--- spec/javascripts/ssh.test.js
+++ spec/javascripts/ssh.test.js
@@ -6,8 +6,29 @@ describe("labelFromPubKey()", () => {
   });
 });
 
+describe("labelFromPubKey()", () => {
+  test("given undefined key, should return empty string", () => {
+    expect(SshModule.labelFromPubKey()).toBe("");
+  });
+});
+
 describe("usernameFromPubKey()", () => {
   test("given empty key, should return empty string", () => {
     expect(SshModule.usernameFromPubKey("")).toBe("");
+  });
+});
+
+describe("usernameFromPubKey()", () => {
+  test("given undefined key, should return empty string", () => {
+    expect(SshModule.usernameFromPubKey()).toBe("");
+  });
+});
+
+describe("sanitizeKey()", () => {
+  test("given undefined key, should return empty string", () => {
+    expect(SshModule.sanitizeKey()).toBe("");
+  });
+  test("given empty key, should return empty string", () => {
+    expect(SshModule.sanitizeKey("")).toBe("");
   });
 });
