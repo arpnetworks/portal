7b15bb7c1021b891ba0e14e2487872cfa28315b1
Garry Dolley <gdolley@arpnetworks.com>
2020-06-09 01:42:39 -0700

    Here too, forget about setting the key type

--- app/models/ssh_host_key.rb
+++ app/models/ssh_host_key.rb
@@ -4,31 +4,12 @@ class SshHostKey < ActiveRecord::Base
   validates :key, presence: true
 
   before_save :generate_fingerprints
-  before_save :set_key_type
 
   protected
 
   def generate_fingerprints
-    
-  end
+    return if key.blank?
 
-  def set_key_type
-    begin
-      key_type = key.split(' ')
 
-      case key_type[0]
-      when 'ssh-rsa'
-        self.key_type = 'rsa'
-      when 'ssh-dsa'
-        self.key_type = 'dsa'
-      when 'ssh-ecdsa'
-        self.key_type = 'ecdsa'
-      when 'ssh-ed25519'
-        self.key_type = 'ed25519'
-      else
-        self.key_type = nil
-      end
-    rescue StandardError => e
-    end
   end
 end
--- spec/models/ssh_host_key_spec.rb
+++ spec/models/ssh_host_key_spec.rb
@@ -3,71 +3,16 @@ require File.dirname(__FILE__) + '/../arp_spec_helper'
 
 context SshHostKey do
   context 'with an SSH host key' do
+
+    # These are rather 'deep' specs (hardly unit tests), but they are
+    # important
     describe 'generate_fingerprints()' do
+      context 'with key' do
+        before do
+        end
+      end
       it 'should generate and save SHA256 fingerprint'
       it 'should generate and save MD5 fingerprint'
-    end
-
-    describe 'set_key_type()' do
-      context 'with RSA key' do
-        before do
-          @ssh_host_key = build(:ssh_host_key, key: 'ssh-rsa AAAA... foo')
-        end
-
-        it 'should set key_type to rsa' do
-          expect(@ssh_host_key.key_type).to be_nil
-          @ssh_host_key.send(:set_key_type)
-          expect(@ssh_host_key.key_type).to eq 'rsa'
-        end
-      end
-
-      context 'with DSA key' do
-        before do
-          @ssh_host_key = build(:ssh_host_key, key: 'ssh-dsa AAAA... foo')
-        end
-
-        it 'should set key_type to dsa' do
-          expect(@ssh_host_key.key_type).to be_nil
-          @ssh_host_key.send(:set_key_type)
-          expect(@ssh_host_key.key_type).to eq 'dsa'
-        end
-      end
-
-      context 'with ECDSA key' do
-        before do
-          @ssh_host_key = build(:ssh_host_key, key: 'ssh-ecdsa AAAA... foo')
-        end
-
-        it 'should set key_type to ecdsa' do
-          expect(@ssh_host_key.key_type).to be_nil
-          @ssh_host_key.send(:set_key_type)
-          expect(@ssh_host_key.key_type).to eq 'ecdsa'
-        end
-      end
-
-      context 'with ED25519 key' do
-        before do
-          @ssh_host_key = build(:ssh_host_key, key: 'ssh-ed25519 AAAA... foo')
-        end
-
-        it 'should set key_type to ed25519' do
-          expect(@ssh_host_key.key_type).to be_nil
-          @ssh_host_key.send(:set_key_type)
-          expect(@ssh_host_key.key_type).to eq 'ed25519'
-        end
-      end
-
-      context 'with unrecognized key' do
-        before do
-          @ssh_host_key = build(:ssh_host_key, key: 'ssh-whatisthis?! AAAA... foo')
-        end
-
-        it 'should not set key_type' do
-          expect(@ssh_host_key.key_type).to be_nil
-          @ssh_host_key.send(:set_key_type)
-          expect(@ssh_host_key.key_type).to be_nil
-        end
-      end
     end
   end
 end
