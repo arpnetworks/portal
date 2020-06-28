e80e9f13298a4bb92c2149675cf56d8c0783bfe6
Garry Dolley <gdolley@arpnetworks.com>
2020-06-27 19:04:30 -0700

    Finished setup_complete email v1

--- spec/mailers/vm_spec.rb
+++ spec/mailers/vm_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe MailerVm, type: :mailer do
       @location = double(:location, display_name_simple: 'Los Angeles')
       allow(@vm).to receive(:account).and_return(@account)
       allow(@vm).to receive(:location).and_return(@location)
+      allow(@vm).to receive(:os).and_return("OpenBSD 6.7")
     end
 
     context 'with mailer' do
@@ -19,6 +20,26 @@ RSpec.describe MailerVm, type: :mailer do
       describe 'setup_complete' do
         it 'should render the subject' do
           expect(@mailer.subject).to eq 'Server Setup Complete'
+        end
+
+        it 'should render the sender email' do
+          expect(@mailer.from).to eql(['support@arpnetworks.com'])
+        end
+
+        it 'should render the receiver email' do
+          expect(@mailer.to).to eql([@account.email])
+        end
+
+        it 'should render OS info' do
+          expect(@mailer.body.encoded).to match(@vm.os)
+        end
+
+        it 'should render account info' do
+          expect(@mailer.body.encoded).to match(@account.display_name)
+        end
+
+        it 'should render newsletter link' do
+          expect(@mailer.body.encoded).to match('arp.serve.sh/newsletter')
         end
       end
     end
