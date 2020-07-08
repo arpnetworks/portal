4bcd6c2838534265104b0f9c20e7f5c522274e01
Garry Dolley <gdolley@arpnetworks.com>
2020-07-08 00:27:52 -0700

    Get jQuery and UJS going again

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,8 @@ require("turbolinks").start()
 // require("@rails/activestorage").start()
 // require("channels")
 
-require("arp/arp.js")
-require("arp/bulma.js")
-require("arp/clipboard.min.js")
-require("arp/ssh.js")
+require("jquery")
+import {} from 'jquery-ujs'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
