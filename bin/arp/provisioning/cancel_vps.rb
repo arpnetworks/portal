a1f14b5dd6a30eff46a82ce55ed8e38311412060
Garry Dolley <gdolley@arpnetworks.com>
2020-07-20 01:32:47 -0700

    Autoformat

--- bin/arp/provisioning/cancel_vps.rb
+++ bin/arp/provisioning/cancel_vps.rb
@@ -17,7 +17,7 @@ end
 # require 'common'
 
 def usage
-  puts "./cancel_vps.rb <UUID>"
+  puts './cancel_vps.rb <UUID>'
 end
 
 def instantiate_ip_blocks(service)
@@ -26,8 +26,8 @@ def instantiate_ip_blocks(service)
   # duplicated in admin/services_controller.rb.  We should find some common
   # place to put it.
   if service.ip_blocks.empty?
-    sc_id = (o = ServiceCode.find_by_name('IP_BLOCK')) && o.id
-    @ip_block_service = @service.account.services.active.find_by_service_code_id(sc_id).ip_blocks
+    sc_id = (o = ServiceCode.find_by(name: 'IP_BLOCK')) && o.id
+    @ip_block_service = @service.account.services.active.find_by(service_code_id: sc_id).ip_blocks
   else
     @ip_block_service = service.ip_blocks
   end
@@ -44,19 +44,17 @@ def ip_blocks_report(ip_blocks)
     s += "IPv#{block.version}: #{block.cidr}\n"
   end
 
-  return s
+  s
 end
 
 def num_of_virtual_machines(account)
   i = 0
 
   account.services.active.each do |service|
-    if service.service_code.name == 'VPS'
-      i += 1
-    end
+    i += 1 if service.service_code.name == 'VPS'
   end
 
-  return i
+  i
 end
 
 def yesno(s)
@@ -75,7 +73,7 @@ end
 @UUID = $ARGV[0]
 @hostname = `hostname`.chomp
 
-@vm = VirtualMachine.find_by_uuid(@UUID)
+@vm = VirtualMachine.find_by(uuid: @UUID)
 
 unless @vm
   puts "Cannot find VM with UUID #{@UUID}"
@@ -95,82 +93,78 @@ instantiate_ip_blocks(@service)
 @vlan = @ip_blocks.first.vlan
 @location = @ip_blocks.first.location.code
 
-puts <<-EOF
-The following VM will be deleted:
+puts <<~EOF
+  The following VM will be deleted:
 
-VM UUID: #{@vm.uuid}
-VM Host: #{@vm.host}
-VM Label: #{@vm.label}
+  VM UUID: #{@vm.uuid}
+  VM Host: #{@vm.host}
+  VM Label: #{@vm.label}
 
-Account Owner: #{@account.display_account_name}
-Account Email: #{@account.email}
+  Account Owner: #{@account.display_account_name}
+  Account Email: #{@account.email}
 
-The following IP blocks are allocated to this customer:
+  The following IP blocks are allocated to this customer:
 
-#{ip_blocks_report(@ip_blocks)}
-VLAN: #{@vlan}
-Location: #{@location}
+  #{ip_blocks_report(@ip_blocks)}
+  VLAN: #{@vlan}
+  Location: #{@location}
 
 EOF
 
-if yesno "Cancel this VPS? [y/N] : "
-  if Rails.env == 'production' && @hostname == $HOST_PORTAL
-    puts ""
+if yesno 'Cancel this VPS? [y/N] : '
+  if Rails.env.production? && @hostname == $HOST_PORTAL
+    puts ''
     puts "Running #{$CANCEL_SCRIPT} on host '#{@vm.host}'..."
     system("ssh #{@vm.host} #{$CANCEL_SCRIPT} #{@vm.uuid}")
 
-    puts ""
+    puts ''
     puts "Running #{$HOST_CACTI_DESTROY_VLAN_SCRIPT}..."
     system("ssh -t -A #{$HOST_RANCID} 'cd #{$HOST_RANCID_DIR} && #{$HOST_CACTI_DESTROY_VLAN_SCRIPT} #{@vlan} #{@location}'")
   end
 
   @ip_blocks.each do |ip_block|
-    if ip_block.version == 4 && ip_block.cidr_obj.bits <= 29
-      puts ""
-      puts "Sending removal SWIP to ARIN for #{ip_block.cidr}..."
+    next unless ip_block.version == 4 && ip_block.cidr_obj.bits <= 29
 
-      @form = OpenStruct.new
-      @form.registration_action = "R"
-      @form.network_name = ip_block.arin_network_name
-      @downstream_org = ip_block.resource.service.account
-      @downstream_org.instance_eval do
-        def public_comments
-          ''
-        end
-      end
+    puts ''
+    puts "Sending removal SWIP to ARIN for #{ip_block.cidr}..."
 
-      if Rails.env == 'production'
-        Mailer.swip_reassign_simple(@form, @downstream_org, ip_block).deliver_now
+    @form = OpenStruct.new
+    @form.registration_action = 'R'
+    @form.network_name = ip_block.arin_network_name
+    @downstream_org = ip_block.resource.service.account
+    @downstream_org.instance_eval do
+      def public_comments
+        ''
       end
     end
+
+    Mailer.swip_reassign_simple(@form, @downstream_org, ip_block).deliver_now if Rails.env.production?
   end
 
-  puts ""
-  puts "Deleting VM service record..."
+  puts ''
+  puts 'Deleting VM service record...'
   @vm.resource.service.destroy
 
   if @ip_blocks.first.resource
-    puts ""
-    puts "Deleting IP service record..."
+    puts ''
+    puts 'Deleting IP service record...'
     @ip_blocks.first.resource.service.destroy
   end
 
   @account.bandwidth_quotas.each do |bq|
-    puts ""
-    puts "Deleting Bandwidth Quota service record..."
+    puts ''
+    puts 'Deleting Bandwidth Quota service record...'
     bq.resource.service.destroy
   end
 
   if @account.mrc == 0
-    puts ""
-    puts "Deleting Credit Card records..."
+    puts ''
+    puts 'Deleting Credit Card records...'
 
     ccs = @account.credit_cards
 
     ccs.each do |cc|
-      if cc
-        cc.destroy
-      end
+      cc&.destroy
     end
   end
 
