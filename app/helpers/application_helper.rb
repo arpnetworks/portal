ab436907bc51abcabcc4a85e38ea74574fd4d23f
Hopper Gee <hopper.gee@hey.com>
2021-12-11 15:57:08 +0800

    Show alert message with a yellow color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
   def render_flash(custom_flash = nil)
     message = custom_flash || flash
-    flash_types = %w(error warning notice)
+    flash_types = %w(error warning notice alert)
     flash_type = flash_types.detect { |a| message.keys.include?(a) }
     if flash_type
       html = "<div id='flash_#{flash_type}_div' class='flash_#{flash_type} rounded'>".html_safe
