3b49383d6f18e0d6d51b2de41e3537cd25e9184d
Garry Dolley <gdolley@arpnetworks.com>
2021-12-16 21:49:43 -0800

    We need to spec against both email MIME types now

--- spec/system/forgot_password_spec.rb
+++ spec/system/forgot_password_spec.rb
@@ -18,30 +18,35 @@ RSpec.describe 'Forgot Password' do
 
     mail = ActionMailer::Base.deliveries.last
     expect(mail.subject).to eq("Reset password instructions")
-    expect(mail.body.to_s).to include("Dear chris@pledie.com,")
-    expect(mail.body.to_s).to include("Someone has requested a link to change your password. You can do this through the link below.")
 
-    matches = mail.body.to_s.scan(/http:\/\/runner:4000(\/accounts\/password\/edit\?reset_password_token=[A-Za-z0-9\-_=]+)\"/)
-    chris_reset_password_path = matches.flatten.first
-    visit chris_reset_password_path
-    expect(page).to have_content("Change your password")
+    # Go through the whole process, for each MIME type of our reset password email
+    mail.parts do |mail|
+      expect(mail.body.to_s).to include("Dear chris@pledie.com,")
+      expect(mail.body.to_s).to include("Someone has requested a link to change your password. You can do this through the link below.")
 
-    fill_in 'account[password]', with: 'newpassword'
-    fill_in 'account[password_confirmation]', with: 'newpassword'
-    click_button "Change my password"
-    expect(page).to have_content("Your password has been changed successfully. You are now signed in.")
-    expect(page).to have_content("chris's dashboard")
-    expect(page.current_path).to eq(dashboard_path)
+      matches = mail.body.to_s.scan(/http:\/\/runner:4000(\/accounts\/password\/edit\?reset_password_token=[A-Za-z0-9\-_=]+)\"/)
+      chris_reset_password_path = matches.flatten.first
 
-    click_link "Logout"
-    expect(page).to have_content("You have been logged out.")
+      visit chris_reset_password_path
+      expect(page).to have_content("Change your password")
 
-    fill_in 'account[login]', with: 'chris'
-    fill_in 'account[password]', with: 'newpassword'
-    click_button "Login"
-    expect(page).to have_content("Welcome chris, it is nice to see you.")
-    expect(page).to have_content("chris's dashboard")
-    expect(page.current_path).to eq(dashboard_path)
+      fill_in 'account[password]', with: 'newpassword'
+      fill_in 'account[password_confirmation]', with: 'newpassword'
+      click_button "Change my password"
+      expect(page).to have_content("Your password has been changed successfully. You are now signed in.")
+      expect(page).to have_content("chris's dashboard")
+      expect(page.current_path).to eq(dashboard_path)
+
+      click_link "Logout"
+      expect(page).to have_content("You have been logged out.")
+
+      fill_in 'account[login]', with: 'chris'
+      fill_in 'account[password]', with: 'newpassword'
+      click_button "Login"
+      expect(page).to have_content("Welcome chris, it is nice to see you.")
+      expect(page).to have_content("chris's dashboard")
+      expect(page.current_path).to eq(dashboard_path)
+    end
   end
 
 end
