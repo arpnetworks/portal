6b9f18454c7b4d5e1f12686c55d1b4fe0c079adf
Garry Dolley <gdolley@arpnetworks.com>
2021-12-24 04:02:45 -0800

    Working on Sales Receipt emails for Stripe users

--- app/models/mailers/stripe.rb
+++ app/models/mailers/stripe.rb
@@ -12,4 +12,22 @@ class Mailers::Stripe < ApplicationMailer
 
     mail(to: @recipients, from: @from, subject: @subject, bcc: @bcc)
   end
+
+  def sales_receipt(account, opts = {})
+    begin
+      @config = YAML.load(File.read(Rails.root + "config/arp/globals.yml"))
+      subject_addition = "; " + @config[Rails.env]['sr_subject']
+    rescue
+      subject_addition = ""
+    end
+
+    @account = account
+    @hosted_invoice_url = opts[:hosted_invoice_url]
+
+    @subject    = "Sales Receipt (#{Time.new.strftime("%b. %Y")})" + subject_addition
+    @from       = "ARP Networks <billing@arpnetworks.com>"
+    @recipients = @account.email_for_sales_receipts
+
+    mail(to: @recipients, from: @from, subject: @subject, bcc: @bcc)
+  end
 end
--- spec/mailers/stripe_spec.rb
+++ spec/mailers/stripe_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Mailers::Stripe, type: :mailer do
       @account = build(:account)
     end
 
-    context 'with mailer' do
+    context 'with payment_failed mailer' do
       before do
         @mailer = Mailers::Stripe.payment_failed(@account)
       end
@@ -49,6 +49,43 @@ RSpec.describe Mailers::Stripe, type: :mailer do
           it 'should render info about Portal login' do
             expect(@mailer.body.encoded).to match('portal.arpnetworks.com')
             expect(@mailer.body.encoded).to match(@account.login)
+          end
+        end
+      end
+    end
+
+    context 'with sales_receipt mailer' do
+      before do
+        @mailer = Mailers::Stripe.sales_receipt(@account)
+      end
+
+      describe 'sales_receipt' do
+        it 'should render the subject' do
+          expect(@mailer.subject).to match 'Sales Receipt'
+        end
+
+        it 'should render the sender email' do
+          expect(@mailer.from).to eql(['billing@arpnetworks.com'])
+        end
+
+        it 'should render the receiver email' do
+          expect(@mailer.to).to eql([@account.email_for_sales_receipts])
+        end
+
+        it 'should render nice greeting' do
+          expect(@mailer.body.encoded).to match("Hi #{@account.display_name}")
+        end
+
+        context 'if hosted invoice URL provided' do
+          before :each do
+            @opts = {
+              hosted_invoice_url: 'https://invoice.stripe.com/i/something-something-something'
+            }
+            @mailer = Mailers::Stripe.sales_receipt(@account, @opts)
+          end
+
+          it 'should render Stripe invoice link' do
+            expect(@mailer.body.encoded).to match('https://invoice.stripe.com/')
           end
         end
       end
