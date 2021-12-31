a832253fbbf0557acbc160b105a143a2046ec624
Garry Dolley <gdolley@arpnetworks.com>
2021-12-31 03:08:00 -0800

    Spec'd and fleshed out all pseudocode

--- app/models/stripe_subscription.rb
+++ app/models/stripe_subscription.rb
@@ -14,61 +14,84 @@ class StripeSubscription
   end
 
   # Adds this service to the customer's subscription in Stripe
-  def add!(service)
-    if current_subscription.nil?
-      ss = Stripe::Subscription.create(customer: @account.stripe_customer_id,
-                                       items: [{
-                                         price: service.stripe_price_id,
-                                         metadata: {
-                                           link_to_service_id: service.id
-                                         }
-                                       }])
+  def add!(service, opts = {})
+    opts[:quantity] ||= 1
 
-      service.stripe_subscription_item_id = first_subscription_item(ss).id
-      service.save
+    if current_subscription.nil?
+      add_service_to_new_subscription!(service, opts)
     else
-      # TODO: A big one!
-      #
-      # subscription = current
-      #   for each line item
-      #   (sub['items']['data'].each do |si|
-      #     price = si.price.id
-      #     qty   = si.quantity
-      #
-      #     if service.stripe_price_id == price
-      #       # We have a hit!
-      #
-      #       # So just call
-      #       Stripe::SubscriptionItem.update(si.id, quantity: qty + 1)
-      #       service.stripe_subscription_item_id = si.id
-      #       service.save
-      #
-      #       return
-      #
-      # Otherwise, no hit, then we create a new item:
-      #
-      #   Stripe::Subscription.update(sub.id, items: [{ price: service.stripe_price_id, metadata: {...} }])
-      #
-      #   # Difficult to pull out the si.id here, so maybe we just wait for it in the callback?
-      #
-      #   Or, wait... how about:
-      #   Stripe::SubscriptionItem.create({
-      #     subscription: sub.id,
-      #     price: service.stripe_price_id,
-      #     // quantity: 1
-      #   })
-      #
-      #   Then the return value has si.id directly
+      add_service_to_subscription!(service, current_subscription, opts)
     end
   end
 
-  def remove!(service)
+  def remove!(service, opts = {})
+    opts[:quantity] ||= 1
+
     # Notes
     #
     # If a quantity goes down to zero, we should SubscriptionItem.delete() it
   end
 
   protected
+
+  def add_service_to_new_subscription!(service, opts)
+    ss = Stripe::Subscription\
+         .create(customer: @account.stripe_customer_id,
+                 items: [{
+                   price: service.stripe_price_id,
+                   quantity: opts[:quantity],
+                   metadata: {
+                     link_to_service_id: service.id
+                   }
+                 }])
+
+    service.stripe_subscription_item_id = first_subscription_item(ss).id
+    service.save
+  end
+
+  def add_service_to_subscription!(service, subscription, opts)
+    @pending_update = nil
+
+    # Iterate through existing subscription items to see if an item already exists of the same
+    # type that we are trying to add
+    subscription_line_items(subscription).each do |si|
+      price_id = si['price']['id']
+      quantity = si['quantity']
+
+      next unless service.stripe_price_id == price_id
+
+      @pending_update = {
+        subscription_item: si,
+        quantity: quantity
+      }
+
+      break
+    end
+
+    si = if @pending_update
+           new_quantity = @pending_update[:quantity] + opts[:quantity]
+
+           # This type of item is already in this subscription, so we just need to
+           # update the quantity
+           Stripe::SubscriptionItem\
+             .update(@pending_update[:subscription_item]['id'],
+                     quantity: new_quantity)
+         else
+           # Otherwise, create a new line item for this subscription
+           Stripe::SubscriptionItem\
+             .create({
+                       subscription: subscription['id'],
+                       price: service.stripe_price_id,
+                       quantity: opts[:quantity],
+                       metadata: {
+                         link_to_service_id: service.id
+                       }
+                     })
+         end
+
+    service.stripe_subscription_item_id = si['id']
+    service.save
+  end
 
   # We assume only one subscription per customer, so, return the first one
   def current_subscription
@@ -77,6 +100,10 @@ class StripeSubscription
   end
 
   def first_subscription_item(subscription)
-    subscription.items.data.first
+    subscription_line_items(subscription).first
+  end
+
+  def subscription_line_items(subscription)
+    subscription['items']['data']
   end
 end
--- spec/models/stripe_subscription_spec.rb
+++ spec/models/stripe_subscription_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'stripe_helper'
 
 RSpec.describe StripeSubscription, type: :model do
   context 'with account' do
@@ -30,8 +31,8 @@ RSpec.describe StripeSubscription, type: :model do
       it 'should create a Customer object in Stripe' do
         @stripe = double Stripe::Customer, id: 'cus_foobar'
 
-        expect(Stripe::Customer).to receive(:create).with(name: @account.display_account_name).\
-          and_return @stripe
+        expect(Stripe::Customer).to receive(:create).with(name: @account.display_account_name)\
+                                                    .and_return @stripe
         expect(@account).to receive(:stripe_customer_id=).with('cus_foobar')
         expect(@account).to receive(:save)
 
@@ -60,6 +61,7 @@ RSpec.describe StripeSubscription, type: :model do
             receive(:create).with(customer: @account.stripe_customer_id,
                                   items: [{
                                     price: @service.stripe_price_id,
+                                    quantity: 1,
                                     metadata: {
                                       link_to_service_id: @service.id
                                     }
@@ -77,20 +79,71 @@ RSpec.describe StripeSubscription, type: :model do
       end
 
       context 'with a prior subscription' do
+        before :each do
+          # The JSON dance is to stringify the keys, which is how Stripe delivers them
+          @current_subscription = JSON.parse(StripeFixtures.subscription.to_json)
+          allow(@ss).to receive(:current_subscription).and_return @current_subscription
+        end
+
         context 'and no item like this is in the current subscription' do
           before :each do
-            # TODO
+            @service.stripe_price_id = 'price_something_new'
           end
 
-          it 'should add it to the current subscription'
+          it 'should add it to the current subscription and save subscription item ID' do
+            @new_stripe_sub_item = double(Stripe::SubscriptionItem)
+            allow(@new_stripe_sub_item).to receive(:[]).with('id').and_return 'si_foo'
+
+            expect(Stripe::SubscriptionItem).to \
+              receive(:create).with(subscription: @current_subscription['id'],
+                                    price: @service.stripe_price_id,
+                                    quantity: 1,
+                                    metadata: {
+                                      link_to_service_id: @service.id
+                                    }).and_return @new_stripe_sub_item
+
+            expect(@service).to receive(:stripe_subscription_item_id=).with('si_foo')
+            expect(@service).to receive(:save)
+            @ss.add!(@service)
+          end
         end
 
         context 'and an existing item like this is already in the current subscription' do
           before :each do
-            # TODO
+            # These values taken inferred from StripeFixtures
+            @service.stripe_price_id = 'price_1KC3FQ2LsKuf8PTnt6V2lJid'
+            @existing_subscription_item = 'si_Ks8cmxDUSgtHga'
+            @new_quantity = 2
+
+            @existing_stripe_sub_item = double(Stripe::SubscriptionItem)
+            allow(@existing_stripe_sub_item).to \
+              receive(:[]).with('id').and_return @existing_subscription_item
           end
 
-          it 'should raise the quantity of the same item within the current subscription'
+          it 'should raise the quantity of the item and save subscription item ID' do
+            expect(Stripe::SubscriptionItem).to \
+              receive(:update).with(@existing_subscription_item, quantity: @new_quantity)\
+                              .and_return @existing_stripe_sub_item
+
+            expect(@service).to receive(:stripe_subscription_item_id=).with(@existing_subscription_item)
+            expect(@service).to receive(:save)
+
+            @ss.add!(@service)
+          end
+
+          context 'and we provide a quantity' do
+            before :each do
+              @quantity_to_add = 5
+            end
+
+            it 'should raise the quantity by specified amount' do
+              expect(Stripe::SubscriptionItem).to \
+                receive(:update).with(@existing_subscription_item, quantity: 6)\
+                                .and_return @existing_stripe_sub_item
+
+              @ss.add!(@service, { quantity: @quantity_to_add })
+            end
+          end
         end
       end
     end
