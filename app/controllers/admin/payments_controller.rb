099f8cc9acab8826b3aab8d28849ea73de12921a
Garry Dolley <gdolley@arpnetworks.com>
2022-01-06 06:58:57 -0800

    Finished preliminary "Record Manual Payment" functionality

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -1,13 +1,13 @@
 class Admin::PaymentsController < Admin::HeadQuartersController
   before_action :is_arp_admin?,     except: [:show]
   before_action :is_arp_sub_admin?, only:   [:show]
-  before_action :find_invoice,      only: %i[new show edit update destroy]
+  before_action :find_invoice,      only: %i[create new show edit update destroy]
   before_action :find_payment,      only: %i[show edit update destroy]
 
-
   def index
-    @payments = Payment.paginate(page: params[:page],
-                                 per_page: params[:per_page] || 20).order('created_at DESC')
+    # @payments = Payment.paginate(page: params[:page],
+                                 # per_page: params[:per_page] || 20).order('created_at DESC')
+    redirect_to admin_invoice_path(params[:invoice_id])
   end
 
   def show; end
@@ -19,23 +19,53 @@ class Admin::PaymentsController < Admin::HeadQuartersController
     render layout: 'responsive'
   end
 
+  def create
+    @payment = @invoice.payments.new(payment_params)
+    @payment.account_id = @invoice.account_id
+
+    if @payment.save!
+      @invoice.payments << @payment
+
+      # This should really go in the model
+      if @invoice.balance == 0
+        @invoice.paid = true
+        @invoice.save
+      end
+
+      flash[:notice] = 'New payment saved'
+      redirect_to(admin_invoice_path(@invoice)) and return
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    render action: 'new', layout: 'responsive'
+  end
+
   protected
 
   def find_invoice
-    begin
-      @invoice = Invoice.find(params[:invoice_id])
-    rescue ActiveRecord::RecordNotFound
-      flash[:error] = "Could not find invoice with ID #{params[:invoice_id]}"
-    end
+    @invoice = Invoice.find(params[:invoice_id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = "Could not find invoice with ID #{params[:invoice_id]}"
+    redirect_to(admin_invoices_path)
   end
 
   def find_payment
-    begin
-      @payment = @invoice.payments.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      flash[:error] =
-        "Could not find payment with ID #{params[:id]} corresponding to invoice with ID #{params[:invoice_id]}"
-      redirect_to(admin_invoices_path)
-    end
+    @payment = @invoice.payments.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] =
+      "Could not find payment with ID #{params[:id]} corresponding to invoice with ID #{params[:invoice_id]}"
+    redirect_to(admin_invoices_path)
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(
+      :date,
+      :reference_number,
+      :check_number,
+      :method,
+      :amount,
+      :notes
+    )
   end
 end
--- spec/controllers/admin/payments_controller_spec.rb
+++ spec/controllers/admin/payments_controller_spec.rb
@@ -47,4 +47,48 @@ describe Admin::PaymentsController do
       end
     end
   end
+
+  describe 'handling POST /admin/invoice/1/payments' do
+    def do_post(opts = {})
+      post :create, params: opts
+    end
+
+    context 'with invoice' do
+      before :each do
+        @invoice = mock_model(Invoice)
+        allow(Invoice).to receive(:find).with(@invoice.id.to_s).and_return @invoice
+
+        @payments = double(:payments)
+        @payment = mock_model(Payment)
+        allow(@invoice).to receive(:payments).and_return @payments
+        allow(@payments).to receive(:new) { @payment }
+        allow(@payment).to receive(:account_id=).with(@invoice.account_id)
+      end
+
+      context 'with valid parameters' do
+        before :each do
+          allow(@payment).to receive(:save!).and_return true
+          allow(@invoice).to receive(:balance)
+        end
+
+        it 'should create payment and assign to invoice' do
+          expect(@payments).to receive(:<<).with(@payment)
+          do_post(invoice_id: @invoice.id, payment: { date: '2022-01-01', amount: 10.00 })
+          expect(flash[:notice]).to eq 'New payment saved'
+          expect(response).to redirect_to(admin_invoice_path(@invoice))
+        end
+      end
+
+      context 'with invalid parameters' do
+        before :each do
+          allow(@payment).to receive(:save!).and_raise ActiveRecord::RecordInvalid
+        end
+
+        it 'should render back to edit screen' do
+          do_post(invoice_id: @invoice.id, payment: { date: '2022-01-01' })
+          expect(response).to render_template('admin/payments/new')
+        end
+      end
+    end
+  end
 end
