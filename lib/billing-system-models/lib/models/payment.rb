0350f78872de827fdb4863628668bdf723c030ef
Garry Dolley <gdolley@arpnetworks.com>
2022-01-07 02:14:06 -0800

    Amount needs to be present and a number; format invoice total nicely

--- lib/billing-system-models/lib/models/payment.rb
+++ lib/billing-system-models/lib/models/payment.rb
@@ -2,4 +2,7 @@ class Payment < ActiveRecord::Base
   belongs_to :account
   has_many :invoices_payments
   has_many :invoices, :through => :invoices_payments
+
+  validates_presence_of :amount
+  validates_numericality_of :amount
 end
