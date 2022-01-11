2d0c83016dfebaa8a9b305d2ab0ce447802db512
Garry Dolley <gdolley@arpnetworks.com>
2022-01-10 23:48:34 -0800

    Configured ActiveJob to use Sidekiq

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,19 @@
 class ApplicationJob < ActiveJob::Base
+  # By default, failed jobs are discarded; by including retry_on, failed
+  # jobs will be retried, before bubbling back up to the queueing system
+  # (Sidekiq); they don't get kicked back to Sidekiq otherwise
+  #
+  # But since we don't have visibility into the internals of ActiveJob,
+  # only attempt twice, then go back to Sidekiq, where we at least have
+  # a dashboard and can check on the jobs
+  retry_on StandardError, wait: 3.seconds, attempts: 2
+
+  # Retry anything that was kick backed to Sidekiq
+  sidekiq_options retry: 5
+
+  # Automatically retry jobs that encountered a deadlock
+  # retry_on ActiveRecord::Deadlocked
+
+  # Most jobs are safe to ignore if the underlying records are no longer available
+  # discard_on ActiveJob::DeserializationError
 end
