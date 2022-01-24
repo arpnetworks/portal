5635f96fd81f520128130dc7378ab6a2c967a190
Garry Dolley <gdolley@arpnetworks.com>
2022-01-24 02:03:04 -0800

    Use an ENV here too

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'snow.arpnetworks.com', roles: %w(app db)
+server ENV['CAP_APP_SERVER'], roles: %w(app db)
 
 # role-based syntax
 # ==================
