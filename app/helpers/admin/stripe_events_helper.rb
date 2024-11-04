76fc98c2bc28f4b577a31d966b7cd24801aaa68f
Garry Dolley <gdolley@arpnetworks.com>
2024-11-03 22:32:46 -0800

    We want the detail for setup_intent too

--- app/helpers/admin/stripe_events_helper.rb
+++ app/helpers/admin/stripe_events_helper.rb
@@ -2,7 +2,7 @@ module Admin::StripeEventsHelper
   def stripe_events_related(stripe_event)
     case stripe_event.event_type
     when 'payment_method.attached', 'invoice.finalized', 'invoice.paid', 'invoice.payment_failed', 'invoice.payment_action_required', 'charge.refunded',
-         'customer.subscription.created'
+         'customer.subscription.created', 'setup_intent.succeeded', 'setup_intent.created'
       account = stripe_event.related(:account)
       account ? link_to(h(account.display_account_name), admin_account_path(account)) : ''
     else
